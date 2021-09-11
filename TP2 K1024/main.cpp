6b69eb046afdee8879cbb14840fe8a9a8fcc33b6
BrunoSalvadorSapoznik <bsapoznik@frba.utn.edu.ar>
2021-09-11 17:15:44 -0300

    Añadir struct usuario compra y string.h

--- TP2 K1024/main.cpp
+++ TP2 K1024/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
 using namespace std;
+#include <string.h>
+struct Usuario
+{
+int IDusuario;
+int FechaCreacion;
+bool Activo;
+float TotalImporteCompras;
+char eMail[26];
+};
+
+struct Compra
+{
+ int CompraID;
+char FechaHora[14];
+float Monto;
+int UsuarioID;
+int NroArticulo;
+
+};
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
